4f2a05872465a258ab7cdee6c8f255b05d7bb1e8
Gemechuhunduma <gemechuhunduma20@gmail.com>
2025-02-19 13:41:28 -0800

    i made the header part of Amazon website and it is responsive�

---
+++ frontend/src/components/header/LowerHeader.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { IoMdMenu } from "react-icons/io";
+import classes from './Header.module.css'
+
+const LowerHeader = () => {
+  return (
+    <>
+      <div className={classes.lower__container}>
+        <ul>
+          <li>
+            <span>
+              <IoMdMenu size={25}/>
+              <p>All</p>
+            </span>
+          </li>
+          <li>Today's Deals</li>
+          <li>Customer Service</li>
+          <li>Registry</li>
+          <li>Gift Card</li>
+          <li>Sell</li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default LowerHeader
